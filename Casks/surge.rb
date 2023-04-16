a82a7bcb51eb4437bdc28af8eac4a2975fbf8398
kosette <35268640+Kosette@users.noreply.github.com>
2023-04-16 13:16:33 +0800

    feat: add surge mac version 4 legacy

---
+++ Casks/surge.rb
@@ -0,0 +1,40 @@
+cask "surge" do
+  version "4.11.1,2013,eac717436d753b5ea2b82a206d5851e7"
+  sha256 "53a0a48463686d11687fbac8f01f0570859d15d3cdd73b251f0041f0ef387ca2"
+
+  url "https://dl.nssurge.com/mac/v#{version.major}/Surge-#{version.tr(",", "-")}.zip"
+  name "Surge"
+  desc "Network toolbox"
+  homepage "https://nssurge.com/"
+
+  livecheck do
+    url "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
+    strategy :sparkle do |item|
+      match = item.url.match(/[._-](\d+(?:\.\d+)+)[._-](\d+)[._-](\h+)\.zip/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]},#{match[3]}"
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Surge.app"
+
+  uninstall launchctl: "com.nssurge.surge-mac.helper",
+            delete:    "/Library/PrivilegedHelperTools/com.nssurge.surge-mac.helper"
+
+  zap delete: [
+    "~/Library/Application Support/Surge",
+    "~/Library/Application Support/com.nssurge.surge-mac",
+    "~/Library/Caches/com.nssurge.surge-mac",
+    "~/Library/Caches/com.nssurge.surge-mac.plist",
+    "~/Library/HTTPStorages/com.nssurge.surge-mac",
+    "~/Library/Logs/Surge",
+    "~/Library/Preferences/com.nssurge.surge-dashboard.plist",
+    "~/Library/Preferences/com.nssurge.surge-mac.plist",
+    "~/Library/Saved Application State/com.nssurge.surge-dashboard.savedState",
+    "~/Library/Saved Application State/com.nssurge.surge-mac.savedState",
+  ]
+end
