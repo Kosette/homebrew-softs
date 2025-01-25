ec3c60229cbbb2dae7882aaec2c21624992cdd32
Kosette <35268640+Kosette@users.noreply.github.com>
2025-01-25 12:04:42 +0800

    Update rimage to v0.10.3

--- Formula/rimage.rb
+++ Formula/rimage.rb
@@ -1,15 +1,15 @@
 class Rimage < Formula
   desc "A tool for resizing images in bulk"
   homepage "https://github.com/SalOne22/rimage"
-  version "0.10.2"
+  version "0.10.3"
   license "MIT"
 
   if Hardware::CPU.intel?
-    url "https://github.com/SalOne22/rimage/releases/download/v0.10.2/rimage-0.10.2-x86_64-apple-darwin.tar.gz"
-    sha256 "a68ce9b168d6af39e510612b402957e1cfff9ce9831e20803d5d2d43b23a344b"
+    url "https://github.com/SalOne22/rimage/releases/download/v0.10.3/rimage-0.10.3-x86_64-apple-darwin.tar.gz"
+    sha256 "48d2605b9ac5bd57a8c895f3b9631b48642dc9ad3ffbf1a76503720a36fc9da0"
   elsif Hardware::CPU.arm?
-    url "https://github.com/SalOne22/rimage/releases/download/v0.10.2/rimage-0.10.2-aarch64-apple-darwin.tar.gz"
-    sha256 "6657b48f65e28eb18a81f622e505df1e6f5630eca1289b7ec4d14e712c558056"
+    url "https://github.com/SalOne22/rimage/releases/download/v0.10.3/rimage-0.10.3-aarch64-apple-darwin.tar.gz"
+    sha256 "c70ede263ea0486000aab8f57f1c8d3a82315f0cd4dda51fbd9c6dc81daabcdb"
   end
 
   def install
